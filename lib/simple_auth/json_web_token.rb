62698af39e2da24bb06ef9b35c6dfb897f7e5d01
zhaolongwen <zlw302323@gmail.com>
2017-11-03 10:48:40 +0800

    [fix bug] add jwt decode algorithm

--- lib/simple_auth/json_web_token.rb
+++ lib/simple_auth/json_web_token.rb
@@ -4,14 +4,14 @@ require 'jwt'
 module SimpleAuth
   module JsonWebToken
     def self.encode(payload)
-      payload[:exp] = SimpleAuth.configuration.exp || 7.days.from_now
+      payload[:exp] = SimpleAuth.configuration.exp.to_i || 7.days.from_now.to_i
       secret_key = SimpleAuth.configuration.secret_key
-      JWT.encode(payload, secret_key)
+      JWT.encode(payload, secret_key, 'HS256')
     end
 
     def self.decode(token)
       secret_key = SimpleAuth.configuration.secret_key
-      body = JWT.decode(token, secret_key)[0]
+      body = JWT.decode(token, secret_key, true, algorithm: 'HS256')[0]
       HashWithIndifferentAccess.new(body)
     rescue StandardError => e
       if e.is_a?(JWT::ExpiredSignature)
